32f4a221dead47c85144d557b0f4f147979c8e91
Renan Cunha <renancdev@gmail.com>
2024-10-30 18:09:18 -0300

    criando operacoes| insercao

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,20 @@
-data class Nodo<T:Any>(var valor:T, var proximoNodo:Nodo<T>? = null){
+data class Nodo<T:Any>(var valor:T, var proximoNodo:Nodo<T>? = null) {
     override fun toString(): String {
-        return if(proximoNodo != null) "$valor -> $proximoNodo" else "$valor"
+        return if (proximoNodo != null) "$valor -> $proximoNodo" else "$valor"
+    }
+}
+
+class operacaoLista<T:Any>{
+    private var inicio: Nodo<T>? = null
+    private var fim: Nodo<T>? = null
+    private var tamanho:Int = 0
+
+    override fun toString(): String = if(tamanho == 0) "lista vazia" else "a lista contém $tamanho itens"
+
+    fun insere(valor:Int):operacaoLista<T> = apply {
+        inicio = Nodo(valor = valor, proximoNodo = inicio)
+        if (fim == null) fim = inicio
+        tamanho++
     }
 }
 
