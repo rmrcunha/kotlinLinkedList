d7d159c11880034627c79b69bfd6a98daefc37f0
Renan Cunha <renancdev@gmail.com>
2024-11-01 05:01:30 -0300

    adicionando insercao ao fim da lista

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,35 @@
 data class Nodo<T:Any>(var valor:T, var proximoNodo:Nodo<T>? = null) {
-    override fun toString(): String {
-        return if (proximoNodo != null) "$valor -> $proximoNodo" else "$valor"
-    }
+    override fun toString(): String = if (proximoNodo != null) "$valor -> $proximoNodo" else "$valor"
 }
 
-class operacaoLista<T:Any>{
+class OperacaoLista<T:Any>{
     private var inicio: Nodo<T>? = null
     private var fim: Nodo<T>? = null
     private var tamanho:Int = 0
 
-    override fun toString(): String = if(tamanho == 0) "lista vazia" else "a lista contém $tamanho itens"
+    override fun toString(): String = if(tamanho == 0) "lista vazia" else "a lista contém $tamanho itens\n${inicio.toString()}"
 
-    fun insere(valor:T):operacaoLista<T> = apply {
+    fun insereComeco(valor:T):OperacaoLista<T> = apply {
         inicio = Nodo(valor = valor, proximoNodo = inicio)
         if (fim == null) fim = inicio
         tamanho++
     }
+
+    fun insereFim(valor:T):OperacaoLista<T> = apply {
+        if(tamanho == 0) return insereComeco(valor)
+
+        val novoNodo = Nodo(valor = valor)
+        fim!!.proximoNodo = novoNodo
+        fim = novoNodo
+        tamanho ++
+    }
 }
 
 fun main(){
-    val lista = Nodo(1, Nodo(2, Nodo( 3,)))
+    var lista = OperacaoLista<Int>()
 
-    println(lista)
+    lista.insereComeco(1).insereComeco(2).insereComeco(3)
+
+    lista.insereFim(1).insereFim(2)
+    print(lista)
 }
